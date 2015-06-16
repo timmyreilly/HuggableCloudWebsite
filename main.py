f2bd12a1d9e8322817dbf9d1ec45656d01cfb74c
timmyreilly <timreilly@live.com>
2015-06-15 20:53:20 -0700

    first commits

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-# main.py
+#main.py
 
-print 'here'
+from flask import Flask
+from flask_sockets import Sockets
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+@sockets.route('/echo')
+def echo_socket(ws):
+    while True:
+        message = ws.receive()
+        ws.send(message)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+if __name__ == "__main__":
+    app.run()
